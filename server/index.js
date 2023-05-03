06944e54009c18a80f8c73d282e29f83bf2c7697
Rogue <prettale4@gmail.com>
2023-05-03 17:34:42 -0600

    added post endpoint to server to add new tasks

--- server/index.js
+++ server/index.js
@@ -6,8 +6,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const { getTasks } = require('./controller.js')
+const { getTasks, createTask } = require('./controller.js')
 
 app.get('/api/tasks', getTasks)
+app.post('/api/tasks', createTask)
 
 app.listen(5478, () => console.log('Docked at port 5478'))
